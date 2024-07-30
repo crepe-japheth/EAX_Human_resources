da5800a0c072c8eb463e044e3e48eea7ee961dde
crepe-japheth <mutabarukajaphett@gmail.com>
2024-07-30 00:34:21 -0700

    added database connection

--- eax_employee_management/settings.py
+++ eax_employee_management/settings.py
@@ -69,28 +69,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'eax_employee_management.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME":"human_resources",
+#         "USER":"root", 
+#         "PASSWORD":"", 
+#         "HOST":"localhost", 
+#         "PORT":"3306" 
 #     }
-# }
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# DATABASES = {
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 # }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME":"human_resources",
-        "USER":"root", 
-        "PASSWORD":"", 
+        "NAME":"eaafwjcc_humanresources", 
+        "USER":"eaafwjcc_humanresource_user", 
+        "PASSWORD":"Infotech!123", 
         "HOST":"localhost", 
         "PORT":"3306" 
     }
