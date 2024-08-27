5cd3a42ad662658ff6945afdc854b75e5e95ffd6
crepe-japheth <mutabarukajaphett@gmail.com>
2024-08-27 01:27:43 -0700

    added hr email on send message

--- employee_leave/signals.py
+++ employee_leave/signals.py
@@ -2,13 +2,14 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .send_email import send_mail_to_employee
 from .models import Employee, CustomUser, Leave
+from decouple import config
 
 
 @receiver(post_save, sender=Leave)
 def leave_created_signal(sender, instance, created, **kwargs):
     if created:
         try:
-            send_mail_to_employee("nshimiyaaron87@gmail.com", instance.leave_date, instance.return_date,instance.status, instance.employee.username, instance.leave_type)
+            send_mail_to_employee(config("HR_EMAIL"), instance.leave_date, instance.return_date,instance.status, instance.employee.username, instance.leave_type)
         except:
             print(f'the leave is being applied {instance.employee.username} - {instance.leave_date}')
     else:
